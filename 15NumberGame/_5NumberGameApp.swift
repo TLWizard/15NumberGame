6176e0b4d5d145bb807194442aed7ed4e78765b8
Tushar Lanka <tusharlanka05@gmail.com>
2025-05-16 00:12:12 -0500

    Initial Commit

---
+++ 15NumberGame/_5NumberGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5NumberGameApp.swift
+//  15NumberGame
+//
+//  Created by Tushar Lanka on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct _5NumberGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 15NumberGameTests/_5NumberGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  _5NumberGameTests.swift
+//  15NumberGameTests
+//
+//  Created by Tushar Lanka on 5/16/25.
+//
+
+import Testing
+@testable import _5NumberGame
+
+struct _5NumberGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
